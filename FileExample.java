71d871f351a3184546ff4ae3ad267047133d3153
vas0011 <156358381+vas0011@users.noreply.github.com>
2024-02-05 16:39:39 -0800

    changes

--- FileExample.java
+++ FileExample.java
@@ -5,51 +5,27 @@ import java.util.List;
 
 public class FileExample {
 
-  /*
+    static List<File> getFiles(File start) throws IOException {
+        List<File> result = new ArrayList<>();
+        if (start.isFile()) {
+            result.add(start);
+        } else if (start.isDirectory()) {
+            File[] files = start.listFiles();
+            if (files != null) {
+                for (File file : files) {
+                    result.addAll(getFiles(file));
+                }
+            }
+        }
+        return result;
+    }
 
-  Given a File (which can represent either a file or directory), return a list
-  of all the files in that directory and all its subdirectories.
+    public static void main(String[] args) throws IOException {
+        File start = new File("folder1/");
+        List<File> files = FileExample.getFiles(start);
 
-  If given the path of a file (rather than a directory), it returns a list with
-  just that element.
-  
-  For example, for the following structure:
-
-  some-files/
-  |-  a.txt
-  |-  more-files/
-      |-  b.txt
-      |-  c.java
-  |-  even-more-files/
-      |-  d.java
-      |-  a.txt
-
-  Given new File("some-files/") as a parameter, we'd expect [some-files/a.txt,
-  some-files/more-files/b.txt, some-files/more-files/c.java,
-  some-files/even-more-files/d.java, some-files/even-more-files/a.txt] as results
-
-  Given new File("some-files/more-files") as a parameter, we'd expect
-  [some-files/more-files/b.txt, some-files/more-files/c.java] as results
-
-  Given new File("some-files/a.txt") as a parameter, we'd expect
-  [some-files/a.txt] and a result
-
-  See the File documentation here: https://docs.oracle.com/javase/8/docs/api/java/io/File.html
-
-  */
-
-	static List<File> getFiles(File start) throws IOException {
-	  File f = start;
-	  List<File> result = new ArrayList<>();
-	  result.add(start);
-	  if(f.isDirectory()) {
-	    File[] paths = f.listFiles();
-	    for(File subFile: paths) {
-	      result.add(subFile);
-	    }
-	  }
-	  return result;
-	}
+        for (File file : files) {
+            System.out.println(file.getPath());
+        }
+    }
 }
-
-
