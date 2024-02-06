b2f09f9f58bf7f921dded7bd5703b999aa9733ea
vas0011 <156358381+vas0011@users.noreply.github.com>
2024-02-05 16:00:56 -0800

    made changes

--- ArrayTests.java
+++ ArrayTests.java
@@ -4,15 +4,15 @@ import org.junit.*;
 public class ArrayTests {
 	@Test 
 	public void testReverseInPlace() {
-    int[] input1 = { 3 };
+    int[] input1 = { 3,4,5,6 };
     ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 3 }, input1);
+    assertArrayEquals(new int[]{6,5,4,3}, input1);
 	}
 
 
   @Test
   public void testReversed() {
-    int[] input1 = { };
-    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    int[] input1 = {3,4,5,6 };
+    assertArrayEquals(new int[]{ 6,5,4,3}, ArrayExamples.reversed(input1));
   }
 }
